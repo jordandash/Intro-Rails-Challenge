77f69e546cbf31b9d6d1ebf7ee5959ae7ab5a632
Jordan Dash <jordan.dash92@gmail.com>
2013-10-25 15:34:31 -0500

    Addded a number of new validations for product objects

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,12 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :price, :stock_quantity, :title
+
+  validates :title, :description, :price, :stock_quantity, 
+  					:presence => true
+
+  validates :stock_quantity, 
+  					:numericality => { :only_integer => true}
+  					
+  validates :price, 
+  					:numericality => { :greater_than_or_equal_to => 0.01}
 end
